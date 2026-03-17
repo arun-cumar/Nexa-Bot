42b3790439ab8f9f692a3d47492da8ceade1e38a
arun•°Cumar <arunkumar773681@gmail.com>
2026-03-17 06:43:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,14 @@
+const config = {
+  OWNER_NAME: ["Arun", "Ansad"],
+  OWNER_NUMBER: ["91xxxxxxxxxx"],
+  BOT_NAME: "NeXa-Bot",
+  DESCRIPTION: "Multi device WhatsApp bot",
+  PREFIX: ".",
+  SESSION_ID: [
+    "SESSION_ID_1",
+    "SESSION_ID_2"
+  ]
 
+}
+
+export default config
