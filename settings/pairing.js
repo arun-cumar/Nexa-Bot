742427c411f642e3dba5ca58cbf933bc9a2db4b2
arun•°Cumar <arunkumar773681@gmail.com>
2026-03-19 19:44:30 +0530

    Update pairing.js

--- settings/pairing.js
+++ settings/pairing.js
@@ -1,8 +1,47 @@
+import chalk from 'chalk';
+import readline from 'readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const question = (text) => new Promise((resolve) => rl.question(text, resolve));
 export async function handlePairing(sock) {
-     if (!sock.authState.creds.registered) {
-        const phoneNumber = await question('\n📞Enter your Phone Number with Country Code (eg: 91xxxx): ');
-        const code = await sock.requestPairingCode(phoneNumber.replace(/[^0-9]/g, ''));
-        console.log(`\x1b[32m\nYOUR 🗝 PAIRING CODE: \x1b[1m${code}\x1b[0m\n`);
+    console.clear();
+    console.log(
+        chalk.redBright(` Nexa-Bot 
+        PAIRING MODE `)
+    );
+
+    if (!sock.authState.creds.registered) {
+
+        //  Input Section
+        const phoneNumber = await question(
+            chalk.redBright('📞 Enter Phone Number: ') +
+            chalk.gray('(eg: 91XXXXXXXXXX) ➤ ')
+        );
+
+        const cleanNumber = phoneNumber.replace(/[^0-9]/g, '');
+
+        console.log(
+            chalk.red('\n⏳ Generating secure pairing code...\n')
+        );
+
+        //  Pairing Code
+        const code = await sock.requestPairingCode(cleanNumber);
+
+        // 🎯 Output Box
+        console.log(
+            chalk.redBright(`
+       🗝 YOUR PAIRING CODE         
+           ${code} `)
+        );
+
+        console.log(
+            chalk.gray('⚡ Use this code in WhatsApp to link your device\n')
+        );
     }
 
-
+    rl.close();
+  }
