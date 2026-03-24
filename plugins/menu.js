eba7d423a9b806e5120f6aaf1c7c58a495cd2cc2
arakbox3 <115354476+arakbox3@users.noreply.github.com>
2026-03-24 11:18:29 +0530

    Update menu.js
    Signed-off-by: arakbox3 <115354476+arakbox3@users.noreply.github.com>

--- plugins/menu.js
+++ plugins/menu.js
@@ -8,25 +8,56 @@ import { sendInteractiveMessage } from '../settings/interactive.js';
 export default async (sock, msg, args) => {
     try {
         const from = msg.key.remoteJid;
+        
         const sender = msg.key.participant || from;
+        
+        const pushName = msg.pushName || "User";
 
-        const commandsDir = path.join(process.cwd(), 'commands');
+        // Runtime / Uptime
+        const runtime = process.uptime();
+        
+        const hours = Math.floor(runtime / 3600);
+        
+        const minutes = Math.floor((runtime % 3600) / 60);
+        
+        const seconds = Math.floor(runtime % 60);
+        
+        const uptime = `${hours}h ${minutes}m ${seconds}s`;
+
+        // Date & Time
+        const date = new Date().toLocaleDateString();
+        
+        const time = new Date().toLocaleTimeString();
+        
+        // Commands Read
+        const commandsDir = path.join(process.cwd(), 'plugins);
+                                      
         const files = fs.readdirSync(commandsDir).filter(file => file.endsWith('.js'));
 
-        // Command list create
+        // Command List Create
         let commandList = '';
+        
+        let totalCommands = 0;
+
         files.forEach((file, index) => {
+            
             const cmdName = file.replace('.js', '');
+            
+            totalCommands++;
+            
             commandList += `${index + 1}. ${config.PREFIX}${cmdName}\n`;
         });
 
-        // design
+        // Random Menu Design
         const randomDesign = menuDesigns[Math.floor(Math.random() * menuDesigns.length)];
 
         const menuText = randomDesign
             .replace('{bot}', config.BOT_NAME)
-            .replace('{user}', sender.split('@')[0])
-            .replace('{date}', new Date().toLocaleDateString())
+            .replace('{user}', pushName)
+            .replace('{date}', date)
+            .replace('{time}', time)
+            .replace('{uptime}', uptime)
+            .replace('{total}', totalCommands)
             .replace('{prefix}', config.PREFIX)
             .replace('{commands}', commandList);
 
@@ -44,6 +75,7 @@ export default async (sock, msg, args) => {
                 buttonParamsJson: JSON.stringify({
                     display_text: "🟢 Alive",
                     id: `${config.PREFIX}alive`
+            
                 })
             }
         ];
@@ -51,11 +83,11 @@ export default async (sock, msg, args) => {
         // Send Interactive Menu
         await sendInteractiveMessage(sock, from, {
             text: menuText,
-            footer: config.BOT_NAME,
+            footer: `© ${config.BOT_NAME}`,
             buttons: buttons
         }, { quoted: msg });
 
     } catch (err) {
-        console.log(err);
+        console.log("Menu Error:", err);
     }
 };
