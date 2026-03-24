02000e747f6b2b77330cb7a45e2e11a9cebe4d39
arakbox3 <115354476+arakbox3@users.noreply.github.com>
2026-03-24 13:06:18 +0530

    Update loader.js
    Signed-off-by: arakbox3 <115354476+arakbox3@users.noreply.github.com>

--- settings/loader.js
+++ settings/loader.js
@@ -1,27 +1,36 @@
 // © 2026 arun•°Cumar. All Rights Reserved.
 import { fquoted } from './settings/quoted.js';
-import menuHandler from '../commands/menu.js';
-import aliveHandler from '../commands/alive.js';
-import pingHandler from '../commands/ping.js';
-import urlHandler from '../commands/url.js';
+import menuHandler from '../plugins/menu.js';
+import aliveHandler from '../plugins/alive.js';
+import pingHandler from '../plugins/ping.js';
+import urlHandler from '../plugins/url.js';
+import stickerHandler from '../plugins/sticker.js';
 
 export default async (commandName, sock, msg, args, extra) => {
     const { isOwner, isAdmin } = extra;
 
     if (commandName === 'menu' || commandName === 'help') {
-        await menuHandler(sock, msg, args);
+        await menuHandler(sock, msg, args, fquoted);
     } 
     
     else if (commandName === 'alive') {
-        await aliveHandler(sock, msg, fquoted);
+        await aliveHandler(sock, msg, args, fquoted);
     }
     
      else if (commandName === 'ping') {
-        await pingHandler(sock, msg, fquoted);
+        await pingHandler(sock, msg, args, fquoted);
     }
      
      else if (commandName === 'url') {
-        await urlHandler(sock, msg, fquoted);
+        await urlHandler(sock, msg, args, fquoted);
+    }
+
+    else if (commandName === 'url') {
+        await urlHandler(sock, msg, args, fquoted);
+    }
+
+    else if (commandName === 'sticker') {
+        await stickerHandler(sock, msg, args, fquoted);
     }
         
         console.log(`Unknown command: ${commandName}`);
