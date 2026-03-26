08e55af03c1348d55fb7ea8233e52d69bd92158a
arakbox3 <115354476+arakbox3@users.noreply.github.com>
2026-03-26 15:23:03 +0530

    Update message.js
    Signed-off-by: arakbox3 <115354476+arakbox3@users.noreply.github.com>

--- message.js
+++ message.js
@@ -14,6 +14,7 @@ export default async (sock, chatUpdate) => {
         if (!msg || !msg.message || msg.key.remoteJid === "status@broadcast") return;
 
         const from = msg.key.remoteJid;
+        const isGroup = from.endsWith('@g.us');
         const sender = msg.key.participant || msg.key.remoteJid;
         const senderNumber = sender.replace(/\D/g, ''); 
 
@@ -64,6 +65,9 @@ export default async (sock, chatUpdate) => {
         console.error("❌ Message Error:", err);    
     }
 };
+
+
+
 
 
 
