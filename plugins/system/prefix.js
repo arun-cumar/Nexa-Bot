f1348bed86675b8720233d8f38801168b323c973
arakbox3 <115354476+arakbox3@users.noreply.github.com>
2026-03-28 19:19:32 +0530

    Create prefix.js
    Signed-off-by: arakbox3 <115354476+arakbox3@users.noreply.github.com>

---
+++ plugins/system/prefix.js
@@ -0,0 +1,26 @@
+import { setPrefix, getPrefix } from '../../lib/settings/prefix.js';
+
+export default async (sock, msg, args) => {
+    const from = msg.key.remoteJid;
+    const currentPrefix = getPrefix();
+
+    // prefix (to check)
+    if (args.length === 0) {
+        return await sock.sendMessage(from, { 
+            text: `Current Prefix is: *${currentPrefix}*` 
+        }, { quoted: msg });
+    }
+
+    // setprefix 
+    const newPrefix = args[0];
+    
+    // Safety checker
+    if (newPrefix.length > 3) {
+        return await sock.sendMessage(from, { text: ".Prefix ! " });
+    }
+
+    setPrefix(newPrefix);
+    await sock.sendMessage(from, { 
+        text: `✅ Prefix updated to: *${newPrefix}*` 
+    }, { quoted: msg });
+};
